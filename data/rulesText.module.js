8064c39381061d0c31f01764e1e895fd6450b4f3
fmagesty <fmagesty@gmail.com>
2022-08-07 18:33:08 -0300

    fea: add rules text data

---
+++ data/rulesText.module.js
@@ -0,0 +1,11 @@
+export const rulesText = (
+  <>
+    <h3>Rules:</h3>
+    <p>- Any live cell with two or three live neighbours survives.</p>
+    <p>- Any dead cell with three live neighbours becomes a live cell.</p>
+    <p>
+      - All other live cells die in the next generation. Similarly, all other dead
+      cells stay dead.
+    </p>
+  </>
+);
