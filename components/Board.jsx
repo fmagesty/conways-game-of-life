6615e2f194a46c9867014551b4d0cbb25a62b132
Felipe <fmagesty@gmail.com>
2022-09-23 13:25:15 -0300

    fea: add initial board with initial cells

--- components/Board.jsx
+++ components/Board.jsx
@@ -1,86 +1,73 @@
-import React, { useState, useEffect } from "react";
-import styles from "../styles/Board.module.css";
+import React, { useState, useEffect } from "react"
+import styles from "../styles/Board.module.css"
 
 export const Board = () => {
-  const [positionX, setPositionX] = useState(0);
-  const [positionY, setPositionY] = useState(0);
-  const [displayedCells, setDisplayedCells] = useState(null);
-  // const [cascadingTextData, setCascadingTextData] = useState();
+	const [currentRound, setCurrentRound] = useState(0)
+	let cellPosArr = []
+	const [initialCells, setInitialCells] = useState(null)
 
-  useEffect(() => {
-    getInitialBoardPositions(0, 500);
-    generateCells();
-    return () => {
-      getInitialBoardPositions(0, 0);
-    };
-  }, []);
+	useEffect(() => {
+		const roundCountdown = setInterval(() => {
+			setCurrentRound((currentRound) => currentRound + 1)
+		}, 1000)
+		getInitialBoardPositions(0, 500, 20)
+		displayInitialCells()
+		return () => clearInterval(roundCountdown)
+	}, [])
 
-  // Mock - TODO: copy this to /utils/data
-  // const cascadingTextata = ["string 1", "string 2", "string 3"];
+	const getInitialBoardPositions = (min, max, initialCellsNum) => {
+		for (let i = 0; i < initialCellsNum; i++) {
+			min = Math.ceil(min)
+			max = Math.floor(max)
+			let x = `${Math.floor(Math.random() * (max - min) + min)}`
+			let y = `${Math.floor(Math.random() * (max - min) + min)}`
+			let cellPositions = { x: x, y: y }
+			cellPosArr.push(cellPositions)
+		}
+	}
 
+	const displayInitialCells = () => {
+		setInitialCells(
+			cellPosArr &&
+				cellPosArr.map((item) => (
+					<rect
+						key={item.x + item.y}
+						x={Math.ceil(item.x / 10) * 10}
+						y={Math.ceil(item.y / 10) * 10}
+						width="10"
+						height="10"
+						fillOpacity="1"
+						strokeDasharray="1"
+						fill="rgb(0, 200, 0)"
+					/>
+				))
+		)
+	}
 
-
-  const generateCells = () => {
-    const cellGroup = [
-    { id: 0, xPosition: 55, yPosition: 10, color: "yellow" },
-    { id: 1, xPosition: 155, yPosition: 100, color: "white" },
-  ];
-    const populateCellGroup = cellGroup.map((item) => {
-      console.log(item);
-    });
-    return populateCellGroup;
-  };
-
-  // for (let i = 0; i < range; i++) {
-  //     console.log(i);
-  //   }
-
-  const getInitialBoardPositions = (min, max) => {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    let x = Math.floor(Math.random() * (max - min) + min);
-    let y = Math.floor(Math.random() * (max - min) + min);
-    setPositionX(`${Math.ceil((x * 10) / 10)}px`);
-    setPositionY(`${Math.ceil((y * 10) / 10)}px`);
-
-    // MOVE THIS OUT - ITS GENERATING THE CELL SVG
-    if (positionX && positionY) {
-      let cellSvg = (
-        <rect
-          x={positionX}
-          y={positionY}
-          width="10"
-          height="10"
-          fillOpacity="1"
-          strokeDasharray="1"
-          fill="whitesmoke"
-        />
-      );
-      setDisplayedCells(cellSvg);
-    }
-  };
-
-  let gridSVG = (
-    <svg width="500" height="500" xmlns="http://www.w3.org/2000/svg">
-      <defs>
-        <pattern
-          id="smallGrid"
-          width="10"
-          height="10"
-          patternUnits="userSpaceOnUse">
-          <path
-            d="M 10 0 L 0 0 0 10"
-            fill="none"
-            stroke="gray"
-            strokeWidth="0.5"
-          />
-        </pattern>
-      </defs>
-      {displayedCells}
-      {/* TODO ON THE FUTURE: MAKE THIS BLINK IN GREEN EVERY SECOND OR SO. IS POSSIBLE MAKE IT A SMOOTH ANIMATION AND NOT LIKE AN EPILEPSY-INDUCING THING IDK THE WORD */}
-      <rect width="100%" height="100%" fill="url(#smallGrid)" />
-    </svg>
-  );
-
-  return <div className={styles.mainContainer}>{gridSVG}</div>;
-};
+	let gridSVG = (
+		<svg width="500" height="500" xmlns="http://www.w3.org/2000/svg">
+			<defs>
+				<pattern
+					id="smallGrid"
+					width="10"
+					height="10"
+					patternUnits="userSpaceOnUse">
+					<path
+						d="M 10 0 L 0 0 0 10"
+						fill="none"
+						stroke="gray"
+						strokeWidth="0.5"
+					/>
+				</pattern>
+			</defs>
+			{initialCells}
+			<rect width="100%" height="100%" fill="url(#smallGrid)" />
+		</svg>
+	)
+	return (
+		<>
+			<p className={styles.currentRoundCounter}>Round: {currentRound}</p>
+			<div className={styles.mainContainer}>{gridSVG}</div>
+		</>
+	)
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,27 +1,17 @@
-import React, { useState, useEffect } from "react";
-import styles from "../styles/Home.module.css";
-import { rulesText } from "../data/rulesText.module.js";
-import { Board } from "../components/Board";
+import React, { useState, useEffect } from "react"
+import styles from "../styles/Home.module.css"
+import { rulesText } from "../data/rulesText.module.js"
+import { Board } from "../components/Board"
 
 export default function Home() {
-  const [currentRound, setCurrentRound] = useState(0);
-
-  useEffect(() => {
-    const roundCountdown = setInterval(() => {
-      setCurrentRound((currentRound) => currentRound + 1);
-    }, 1000);
-    return () => clearInterval(roundCountdown);
-  }, []);
-
-  return (
-    <>
-      <div className={styles.mainContainer}>
-        <h1 className={styles.title}>-- Conway&apos;s Game of Life app --</h1>
-        <span>v1</span>
-        <div className={styles.rulesTextContainer}>{rulesText}</div>
-        <p className={styles.currentRoundCounter}>Round: {currentRound}</p>
-        <Board />
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className={styles.mainContainer}>
+				<h1 className={styles.title}>-- Conway&apos;s Game of Life app --</h1>
+				<span>v1</span>
+				<div className={styles.rulesTextContainer}>{rulesText}</div>
+				<Board />
+			</div>
+		</>
+	)
 }
