1006906b119c561cb5330e4fb1d7293615778520
melipefelgaco <fmagesty@gmail.com>
2024-05-16 11:59:51 -0300

    update css

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,17 +1,18 @@
-import React, { useState, useEffect } from "react"
-import styles from "../styles/Home.module.css"
-import { rulesText } from "../data/rulesText.module.js"
-import { Board } from "../components/Board"
+import React, { useRef } from "react";
+import styles from "../styles/Home.module.css";
+import { rulesText } from "../data/rulesText.module.js";
+import { Board } from "../components/Board";
 
 export default function Home() {
 	return (
 		<>
 			<div className={styles.mainContainer}>
 				<h1 className={styles.title}>-- Conway&apos;s Game of Life app --</h1>
-				<span>v1</span>
-				<div className={styles.rulesTextContainer}>{rulesText}</div>
-				<Board />
+				<div className={styles.gameContainer}>
+					<Board  />
+					<div className={styles.rulesTextContainer}>{rulesText}</div>
+				</div>
 			</div>
 		</>
-	)
+	);
 }
